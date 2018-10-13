f7e82b2dfc6c2b695b178c4c248dc3765c121611
anshu <anshu.chaudhari@gmail.com>
2018-10-13 16:55:21 -0700

    Changed functional component to class component, performed AJAX call and rendered response data from component.

--- src/App.js
+++ src/App.js
@@ -1,49 +1,16 @@
 import React, {Component} from "react";
-import axios from "axios";
 import DataTableList from './DataTableList';
+
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: [],
             tableFlag: false
         };
     }
 
-    componentDidMount() {
-        let proxyUrl = "https://cors-anywhere.herokuapp.com/",
-            targetUrl = "https://istheapplestoredown.com/api/v1/status/worldwide";
-
-        axios
-            .get(proxyUrl + targetUrl)
-            .then(response => {
-                let responseArray = [];
-
-                let obj = response.data;
-                //console.log(obj);
-                for (let prop in obj) {
-                    if (obj.hasOwnProperty(prop)) {
-                        //console.log(prop, obj[prop].name);
-                        let infoObject = {
-                            countryCode: prop,
-                            countryName: obj[prop].name,
-                            countryStatus: obj[prop].status
-                        };
-                        responseArray.push(infoObject);
-                    }
-                }
-                console.log(responseArray);
-                this.setState({
-                    data: responseArray
-                });
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    }
-
-    toggleShow = () => {
+    toggleTable = () => {
         this.setState({
             tableFlag: true
         });
@@ -59,15 +26,13 @@ class App extends Component {
                             type="text"
                             name="username"
                             placeholder="Enter Username"
-                            //value={username}
                         />
                         <input
                             type="password"
                             name="password"
                             placeholder="Enter Password"
-                            //value={lname}
                         />
-                        <input type="button" name="submit" value="Submit" onClick={this.toggleShow}/>
+                        <input type="button" name="submit" value="Submit" onClick={this.toggleTable}/>
                     </form>
 
                     : null
@@ -79,8 +44,6 @@ class App extends Component {
         );
     }
 }
-
-
 
 
 export default App;
--- src/DataTableList.js
+++ src/DataTableList.js
@@ -1,34 +1,77 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios/index";
 
-const DataTableList = (props) => (
 
-    <div className="container">
-        <div className="row">
-            <div className="col-md-8 col-md-offset-2">
-                <table className="table table-bordered table-hover">
-                    <thead>
-                    <tr>
-                        <td>Country Code</td>
-                        <td>Country Name</td>
-                        <td>Store Status</td>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {props.data.map((object, index) =>
-                        <tr key={index}>
-                            <td>{object.countryCode}</td>
-                            <td>{object.countryName}</td>
-                            <td>{object.countryStatus}</td>
-                        </tr>
-                    )}
+class DataTableList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
+        };
+    }
 
-                    </tbody>
+    componentDidMount() {
 
-                </table>
+        //The origin does not support cors, so we need to pass it through a proxyURL which makes it cors compatible
+        let proxyUrl = "https://cors-anywhere.herokuapp.com/",
+            targetUrl = "https://istheapplestoredown.com/api/v1/status/worldwide";
+
+        axios
+            .get(proxyUrl + targetUrl)
+            .then(response => {
+                let responseArray = [];
+                let obj = response.data;
+                for (let prop in obj) {
+                    if (obj.hasOwnProperty(prop)) {
+                        let infoObject = {
+                            countryCode: prop,
+                            countryName: obj[prop].name,
+                            countryStatus: obj[prop].status
+                        };
+                        responseArray.push(infoObject);
+                    }
+                }
+                this.setState({
+                    data: responseArray
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    render() {
+
+        return (
+           <div className="container">
+                <div className="row">
+                    <div>
+                        <table className="table table-bordered table-hover">
+                            <thead>
+                            <tr>
+                                <td>Country Code</td>
+                                <td>Country Name</td>
+                                <td>Store Status</td>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {this.state.data.map((object, index) =>
+                                <tr key={index}>
+                                    <td>{object.countryCode}</td>
+                                    <td>{object.countryName}</td>
+                                    <td>{object.countryStatus}</td>
+                                </tr>
+                            )}
+
+                            </tbody>
+
+                        </table>
+                    </div>
+                </div>
+
             </div>
-        </div>
-
-    </div>
-);
+        )
+    }
+}
 
 export default DataTableList;
