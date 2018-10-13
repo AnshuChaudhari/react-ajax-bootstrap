a738192e25335aa10d02c90b71275eaf9f5a0892
anshu <anshu.chaudhari@gmail.com>
2018-10-13 13:40:50 -0700

    Added Ajax call and prepared the Data to render

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    let proxyUrl = "https://cors-anywhere.herokuapp.com/",
+      targetUrl = "https://istheapplestoredown.com/api/v1/status/worldwide";
+
+    axios
+      .get(proxyUrl + targetUrl)
+      .then(response => {
+        let responseArray = [];
+
+        let obj = response.data;
+        //console.log(obj);
+        for (let prop in obj) {
+            if(obj.hasOwnProperty(prop)){
+                //console.log(prop, obj[prop].name);
+                let infoObject = {
+                    countryCode: prop,
+                    countryName: obj[prop].name,
+                    countryStatus: obj[prop].status
+                };
+                responseArray.push(infoObject);
+            }
+        }
+        console.log(responseArray);
+        this.setState({
+          data: responseArray
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <form>
+          <input
+            type="text"
+            name="username"
+            placeholder="Enter Username"
+            //value={username}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter Password"
+            //value={lname}
+          />
+          <input type="button" name="submit" value="Submit" />
+        </form>
       </div>
     );
   }
