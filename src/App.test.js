8e92ff52db0d348a524baa37a326ab85be976b52
anshu <anshu.chaudhari@gmail.com>
2018-10-14 19:43:47 -0700

    removed unwanted files

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import Enzyme, { shallow, mount } from "enzyme";
+import App from "./components/LoginApp/LoginApp";
+import Adapter from "enzyme-adapter-react-16";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("App components", () => {
+    test("renders", () => {
+        const wrapper = shallow(<App />);
+
+        expect(wrapper.exists()).toBe(true);
+    });
+
+    test("user text is echoed", () => {
+        const wrapper = shallow(<App />);
+
+         wrapper.find("#username").simulate("change", {
+            target: { value: "hello" }
+        });
+        //console.log(container.state());
+        expect(wrapper.state().username).toEqual("hello");
+    });
+
+/*    test("when the form is submitted the event is called", () => {
+        const wrapper = shallow(<App />);
+        let prevented = false;
+        wrapper.find("form").simulate("submit", {
+            preventDefault: () => {
+                prevented = true;
+            }
+        });
+        expect(prevented).toBe(true);
+    });
+    */
 });
